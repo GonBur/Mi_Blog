e11290c1a3d73217762abe0fb1a8080487a86ee1
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-03-14 21:03:01 -0500

    actualizando cambios 1.3

--- javaScript/index.js
+++ javaScript/index.js
@@ -8,3 +8,23 @@ function mostrarMenu(){
 }
 
 boton.addEventListener("click", mostrarMenu)
+
+
+
+/* repoducir audio */
+let botonMusica = document.getElementById("botonMusica")
+
+function playAudio(){
+    document.getElementById("audioPrincipal").play();
+}
+
+botonMusica.addEventListener("click", playAudio)
+
+
+/* pausar audio */
+let botonPause = document.getElementById("botonPause")
+
+function pause(){
+    document.getElementById("audioPrincipal").pause();
+}
+botonPause.addEventListener("click", pause)
