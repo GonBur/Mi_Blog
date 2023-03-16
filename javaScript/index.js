0ed046883637a781dc358763fe80616ec1f658ee
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-03-15 23:24:08 -0500

    terminando la pagina principal de mi blog

--- javaScript/index.js
+++ javaScript/index.js
@@ -13,37 +13,16 @@ boton.addEventListener("click", mostrarMenu)
 /* repoducir audio */
 let botonMusica = document.getElementById("botonMusica")
 let audio = document.getElementById("audioPrincipal");
-audio.play();
 
 function playAudio(){
     
     if (audio.paused) {
         audio.play();
-        botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_up</span>";
+        botonMusica.innerHTML = "<span class='material-symbols-outlined' style='font-size: 4rem;';>volume_up</span>";
     } else {
         audio.pause();
-        botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_off</span>";
+        botonMusica.innerHTML = "<span class='material-symbols-outlined' style='font-size: 4rem;';>volume_off</span>";
     }
 }
 
 botonMusica.addEventListener("click", playAudio)
-
-
-
-
-
-
-/* let audio = document.getElementsByTagName("audio")[0];
-let botonMusica = document.getElementById("botonMusica");
-
-function repoducirMusica() {
-  if (audio.paused) {
-    audio.play();
-    botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_up</span>";
-  } else {
-    audio.pause();
-    botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_off</span>";
-  }
-}
-
-botonMusica.addEventListener("click", repoducirMusica) */
