da9047730c11db169d2d9361c634b87e248b4700
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-03-15 20:28:15 -0500

    acutalizando

--- javaScript/index.js
+++ javaScript/index.js
@@ -12,19 +12,34 @@ boton.addEventListener("click", mostrarMenu)
 
 
 /* repoducir audio */
-let botonMusica = document.getElementById("botonMusica")
+/* let botonMusica = document.getElementById("botonMusica")
 
 function playAudio(){
     document.getElementById("audioPrincipal").play();
 }
 
-botonMusica.addEventListener("click", playAudio)
+botonMusica.addEventListener("click", playAudio) */
 
 
 /* pausar audio */
-let botonPause = document.getElementById("botonPause")
+/* let botonPause = document.getElementById("botonPause")
 
 function pause(){
     document.getElementById("audioPrincipal").pause();
 }
-botonPause.addEventListener("click", pause)
+botonPause.addEventListener("click", pause) */
+
+let audio = document.getElementsByTagName("audio")[0];
+let botonMusica = document.getElementById("botonMusica");
+
+function repoducirMusica() {
+  if (audio.paused) {
+    audio.play();
+    botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_up</span>";
+  } else {
+    audio.pause();
+    botonMusica.innerHTML = "<span class='material-symbols-outlined'>volume_off</span>";
+  }
+}
+
+botonMusica.addEventListener("click", repoducirMusica)
